23762a7c1976bb23ca9696395106e4e1297b5a8f
nabd0018 <nabd0018@student.monash.edu>
2022-04-14 16:14:50 +0200

    changed base directory

--- functions.py
+++ functions.py
@@ -11,8 +11,6 @@ This file contains the helper function to be called from main.py
 """
 
 # importing the necessary libraries for working with json
-# import json
-import os
 from tkinter import Frame, Label, Entry, X, LEFT, RIGHT, YES, messagebox, Button, Tk, TOP
 
 # declaring the constants to be used everywhere in the module
@@ -22,9 +20,12 @@ FIELDS = ('KEYMAPOPTS', 'HOSTNAMEOPTS', 'INTERFACESOPTS', 'DNSOPTS', 'TIMEZONEOP
 DISPlAY_FIELDS = ('Keyboard Layout', 'Host Name', 'Network Interface', 'DNS and Domain', 'Timezone', 'Proxy',
                   'Repository', 'SSH Server', 'NTP Service', 'DISKOPTS', 'LBUOPTS', 'APKCACHEOPTS')
 
-os.getcwd()
-ANSWERS_FILENAME = os.getcwd() + "/tmp/answers.txt"
-# print(ANSWERS_FILENAME)
+BASE_DIR = "/usr/local/KC"
+
+ANSWERS_FILE = BASE_DIR + "/config/answers.txt"
+HOST_FILE = BASE_DIR + "/etc/hostname.txt"
+INTERFACES_FILE = BASE_DIR + "/etc/network/interfaces.txt"
+RESOLVE_FILE = BASE_DIR + "/etc/resolve.conf"
 
 global txt_result
 global my_ents
@@ -68,12 +69,8 @@ def read_txt_to_lst(filename):
     :return: list containing the data
     """
     mylines = []
-    # of = open(filename, 'rt')
-    # s = of.readlines()
-    # print(s)
     with open(filename, 'rt') as myfile:
         for myline in myfile:
-            # print(myline)
             if myline.startswith(FIELDS[0]) or myline.startswith(FIELDS[1]) or myline.startswith(
                     FIELDS[2]) or \
                     myline.startswith(FIELDS[3]) or myline.startswith(FIELDS[4]) or myline.startswith(FIELDS[5]) \
@@ -83,7 +80,6 @@ def read_txt_to_lst(filename):
                     myline.startswith(FIELDS[11]):
                 start = myline.find("\"")
                 end = myline.rfind("\"")
-                # print(myline[start+1:end])
                 if myline[start + 1:end].startswith("-"):
                     mylines.append(myline[start + 4:end])
                 else:
@@ -101,8 +97,7 @@ def make_form(root, fields):
     make_label(root)
     entries = {}
     i = 0
-    # data = read_txt_to_lst(ANSWERS_FILENAME)
-    data = read_to_dict(ANSWERS_FILENAME)
+    data = read_to_dict(ANSWERS_FILE)
     for field in fields:
         row = Frame(root)
         lab = Label(row, width=22, text=DISPlAY_FIELDS[i] + ": ", anchor='w')
@@ -111,7 +106,6 @@ def make_form(root, fields):
         else:
             ent = Entry(row)
             if data[field].startswith("-"):
-                # print(data[field][3:])
                 ent.insert(0, data[field][3:])
             else:
                 ent.insert(0, data[field])
@@ -130,8 +124,7 @@ def read(ents):
     :param ents: entries to re populate
     :return: void
     """
-    # data = read_txt_to_lst(ANSWERS_FILENAME)
-    data = read_to_dict(ANSWERS_FILENAME)
+    data = read_to_dict(ANSWERS_FILE)
     for i in range(len(FIELDS)):
         ents[FIELDS[i]].delete(0, 'end')
         if data[FIELDS[i]].startswith("-"):
@@ -154,32 +147,6 @@ def make_label(root):
     return
 
 
-# def read_from_json(filename):
-#     """
-#     This function reads the previous users data from a .json file that can contain 0 or more json objects
-#     (stored as an array of json objects)
-#     :param filename: file to get the records from
-#     :return: an array of json objects
-#     """
-#     input_file = open(filename)
-#     json_array = json.load(input_file)
-#     user_list = []
-#
-#     for user in json_array:
-#         user_details = {FIELDS[0]: None, FIELDS[1]: None, FIELDS[2]: None, FIELDS[3]: None,
-#                         FIELDS[4]: None, FIELDS[5]: None, FIELDS[6]: None, FIELDS[7]: None,
-#                         FIELDS[8]: None, FIELDS[9]: None, FIELDS[10]: None, FIELDS[0]: user[FIELDS[0]],
-#                         FIELDS[1]: user[FIELDS[1]], FIELDS[2]: user[FIELDS[2]],
-#                         FIELDS[3]: user[FIELDS[3]], FIELDS[4]: user[FIELDS[4]],
-#                         FIELDS[5]: user[FIELDS[5]], FIELDS[6]: user[FIELDS[6]],
-#                         FIELDS[7]: user[FIELDS[7]], FIELDS[8]: user[FIELDS[8]],
-#                         FIELDS[9]: user[FIELDS[9]], FIELDS[10]: user[FIELDS[10]]}
-#
-#         user_list.append(user_details)
-#
-#     return user_list
-
-
 def submit(entries):
     """
     This function is executed when the user fills in all the inforamtion and clicks submit.
@@ -188,37 +155,13 @@ def submit(entries):
     :return: void
     """
     cont = True
-    # for i in range(len((entries))):
-    #     if entries[FIELDS[i]].get() == "":
-    #         cont = False
-    #         txt_result.config(
-    #             text="Please complete the required field!", fg="red")
-    # if entries[FIELDS[3]].get() != entries[FIELDS[4]].get():
-    #     cont = False
-    #     txt_result.config(text="Passwords do not match!", fg="red")
 
     if cont:
         my_dict = {}
         for i in range(len(entries)):
             my_dict[FIELDS[i]] = entries[FIELDS[i]].get()
-        # print(dict)
 
-        #
-        # users_list = []
-        # if os.path.exists(USERS_FILENAME) and os.stat(USERS_FILENAME).st_size != 0:
-        #     users_list = read_from_json(USERS_FILENAME)
-        #     users_lisxt.append(dict)
-        # else:
-        #     users_list.append(dict)
-
-        # print(dict)
-
-        # filename = str(entries[FIELDS[0]].get()).replace(" ", "") + ".json"
-        # filename = "/Users/nadeem/Documents/Khwarizm/Alpine/alpine-install/records/" + filename
-        filename = ANSWERS_FILENAME
-        # with open(filename,
-        #           "w") as write_file:  # change "w" to "a" if you want to append instead of overwrite
-        #     json.dump(dict, write_file, indent=4)
+        filename = ANSWERS_FILE
         comments = ["# Example answer file for setup-alpine script\n"
                     "# If you don't want to use a certain option, then comment it out\n\n"
                     "# Use US layout with US variant\n",
@@ -255,42 +198,11 @@ def submit(entries):
                     file.write(FIELDS[i] + "=\"" + my_dict[FIELDS[i]] + "\"")
                 file.write("\n")
             file.write("\n")
-        # jsonString = json.dumps(users_list, indent=4)
-        # jsonFile = open(USERS_FILENAME, "w")
-        # jsonFile.write(jsonString)
-        # jsonFile.close()
 
         txt_result.config(text="Successfully submitted data!", fg="green")
 
         clear(entries, True)
 
-    """
-    After submitting
-    
-    Enable and run dbus for GUI
-    
-    # rc-service dbus start
-    # rc-update add dbus
-    
-    Enable and run lxdm
-    
-    # rc-service lxdm start
-    # rc-update add lxdm
-    
-    import os
-
-    cmd = 'rc-service dbus start'
-    os.system(cmd)
-    
-    cmd = 'rc-update add dbus'
-    os.system(cmd)
-    
-    cmd = 'rc-service lxdm start'
-    os.system(cmd)
-    
-    cmd = 'rc-update add lxdm'
-    os.system(cmd)
-    """
     return
 
 
@@ -362,7 +274,6 @@ def initialise_window():
     my_ents = make_form(my_root, FIELDS)
     my_root.geometry("800x600")
     my_root.title("Khwarizm Consulting")
-    # root.bind('<Return>', (lambda event, e=ents: fetch(e)))
     return my_root
 
 
